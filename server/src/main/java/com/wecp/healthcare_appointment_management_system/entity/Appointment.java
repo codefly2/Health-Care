2836a54adf3447f895f4fda6ae91a44a6655cfb2
username <wecp-dummy@gmail.com>
2025-09-28 20:56:09 +0000

    do it

--- server/src/main/java/com/wecp/healthcare_appointment_management_system/entity/Appointment.java
+++ server/src/main/java/com/wecp/healthcare_appointment_management_system/entity/Appointment.java
@@ -8,7 +8,7 @@ public class Appointment {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer tId;
+    private Integer Id;
     
     @ManyToOne
 
@@ -20,4 +20,45 @@ public class Appointment {
     private LocalDateTime appointmentTime;
     
     private String status; // 'Scheduled', 'Completed', 'Canceled'
+
+    public Integer getId() {
+        return Id;
+    }
+
+    public void setId(Integer id) {
+        Id = id;
+    }
+
+    public Patient getPatient() {
+        return patient;
+    }
+
+    public void setPatient(Patient patient) {
+        this.patient = patient;
+    }
+
+    public Doctor getDoctor() {
+        return doctor;
+    }
+
+    public void setDoctor(Doctor doctor) {
+        this.doctor = doctor;
+    }
+
+    public LocalDateTime getAppointmentTime() {
+        return appointmentTime;
+    }
+
+    public void setAppointmentTime(LocalDateTime appointmentTime) {
+        this.appointmentTime = appointmentTime;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
 }
